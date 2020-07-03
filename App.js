0e75bde3e178a4f7cf1ab38654eb96aedd135be3
B-raw <michaelbarry1989@hotmail.com>
2020-07-03 15:32:06 +0100

    createStackNavigator boilerplate and created SearchScreen

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search'
+    }
+  }
+);
+
+// app.js file is a special file - when app first starts up, anything exported from this file will be taken and shown on screen of device. In this case, we're not exporting a component per se, we're exporting a React component created by "CreateAppContainer" with whatever navigator needs to show.
+export default createAppContainer(navigator);
